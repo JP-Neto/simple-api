c1a162a2383be50dd53cbf570aed0487534c7bf7
Jose Neto <113240030+JP-Neto@users.noreply.github.com>
2026-02-20 00:41:09 -0300

    fix: Ajustes Router

--- infrastructure/04-application/data.tf
+++ infrastructure/04-application/data.tf
@@ -1,16 +1,16 @@
 data "terraform_remote_state" "connectivity" {
   backend = "s3"
   config = {
-    bucket  = "jpn-tfstate-s3"
-    key     = "connectivity/connectivity.tfstate"
+    bucket = "jpn-tfstate-s3"
+    key    = "jpn/development/connectivity.tfstate"
     region = "us-east-1"
   }
 }
 data "terraform_remote_state" "security" {
   backend = "s3"
   config = {
-   bucket  = "jpn-tfstate-s3"
-   key     = "security/security.tfstate"
+    bucket = "jpn-tfstate-s3"
+    key    = "jpn/development/security.tfstate"
     region = "us-east-1"
   }
 }
