f3f851181fdc69a086f0fdad4faa3bfde2ff1d1d
Jose Neto <113240030+JP-Neto@users.noreply.github.com>
2026-02-23 17:39:32 -0300

    Destroy All Resources

--- infrastructure/02-security/ouputs.tf
+++ infrastructure/02-security/ouputs.tf
@@ -1,30 +1,30 @@
-# #----------# Security Group ECS Outputs #-----------#
+# # #----------# Security Group ECS Outputs #-----------#
 
-output "sg_ecs_id" {
-  description = "O ID do Security Group do ECS"
-  value       = module.sg_ecs.security_group_id
-}
+# output "sg_ecs_id" {
+#   description = "O ID do Security Group do ECS"
+#   value       = module.sg_ecs.security_group_id
+# }
 
-# #----------# Security Group RDS Postgres Outputs #-----------#
+# # #----------# Security Group RDS Postgres Outputs #-----------#
 
-output "sg_rds_postgres_id" {
-  description = "O ID do Security Group do RDS Postgres"
-  value       = module.sg_rds_postgres.security_group_id
-}
+# output "sg_rds_postgres_id" {
+#   description = "O ID do Security Group do RDS Postgres"
+#   value       = module.sg_rds_postgres.security_group_id
+# }
 
-#----------# Outputs para o Remote State #-----------#
-output "ecs_task_execution_role_arn" {
-  value = module.ecs_execution_role.role_arn
-}
+# #----------# Outputs para o Remote State #-----------#
+# output "ecs_task_execution_role_arn" {
+#   value = module.ecs_execution_role.role_arn
+# }
 
-output "ecs_task_role_arn" {
-  value = module.ecs_task_role.role_arn
-}
+# output "ecs_task_role_arn" {
+#   value = module.ecs_task_role.role_arn
+# }
 
-output "lambda_deploy_role_arn" {
-  value = module.lambda_deploy_role.role_arn
-}
+# output "lambda_deploy_role_arn" {
+#   value = module.lambda_deploy_role.role_arn
+# }
 
-output "codedeploy_role_arn" {
-  value = module.codedeploy_role.role_arn
-}
+# output "codedeploy_role_arn" {
+#   value = module.codedeploy_role.role_arn
+# }
